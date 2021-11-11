b0b5208e3f955441acef2c9e88744e65c77b1c3b
Guntis Liepins <turbiina@gmail.com>
2021-11-11 14:50:51 +0200

    testing version

--- nutanix_api-list.py
+++ nutanix_api-list.py
@@ -15,7 +15,7 @@ username=input("Nutanix username:")
 password=getpass("Nutanix password:")
 max_results=99999       #limits maximum results. Not needed for small installs.
 api=NutanixAPI(URL,username,password,"/tmp/nutanix_api.log",logging.DEBUG,False)
-#and here we are practicing
+
 
 print("### CLUSTERS ###")
 api.list_clusters_screen() #list subnets
